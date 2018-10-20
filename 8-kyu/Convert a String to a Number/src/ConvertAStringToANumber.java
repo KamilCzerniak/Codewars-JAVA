75592df468d3700d5eda79ceba846c45e657f490
KamilCzerniak <kamil.z.czerniak@gmail.com>
2018-10-21 01:02:05 +0200

    Complited

---
+++ 8-kyu/Convert a String to a Number/src/ConvertAStringToANumber.java
@@ -0,0 +1,9 @@
+public class ConvertAStringToANumber {
+    public static int stringToNumber(String str) {
+        return Integer.parseInt(str);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
