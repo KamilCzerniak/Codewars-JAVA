fa8df4d51b59b57c416d40d6871671760c3106aa
KamilCzerniak <kamil.z.czerniak@gmail.com>
2018-10-21 22:35:56 +0200

    Completed

---
+++ 8-kyu/Function 3 - multiplying two numbers/src/Function3MultiplyingTwoNumbers.java
@@ -0,0 +1,9 @@
+public class Function3MultiplyingTwoNumbers {
+
+    public static int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+    public static void main(String[] args) {
+
+    }
+}
