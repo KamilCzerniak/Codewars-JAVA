52b2033a2709f588874827f1da1c9dcf1fcfcbd2
KamilCzerniak <kamil.z.czerniak@gmail.com>
2018-10-21 21:56:24 +0200

    Completed

---
+++ 8-kyu/Century From Year/src/CenturyFromYear.java
@@ -0,0 +1,10 @@
+public class CenturyFromYear {
+
+    public static int century(int number) {
+        return (int) Math.ceil((double) number / 100);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
