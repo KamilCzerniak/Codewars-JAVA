244ff72e1bd70132f6e9ea8c16fc5035c44541bf
KamilCzerniak <kamil.z.czerniak@gmail.com>
2018-10-23 00:22:48 +0200

    Completed

---
+++ 8-kyu/DNA to RNA Conversion/src/DNAToRNAConversion.java
@@ -0,0 +1,10 @@
+public class DNAToRNAConversion {
+
+    public String dnaToRna(String dna) {
+        return dna.replaceAll("T", "U");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
