40bcca7dbfd10b81eb5a2c9dd9a6658f77a102d1
KamilCzerniak <kamil.z.czerniak@gmail.com>
2018-10-23 00:17:40 +0200

    Completed

---
+++ 8-kyu/Volume of a Cuboid/src/VolumeOfACuboid.java
@@ -0,0 +1,10 @@
+public class VolumeOfACuboid {
+
+    public static double getVolumeOfCuboid(final double length, final double width, final double height) {
+        return length * width * height;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
