2e2dc4f3435023ba8ea838fefd5972a056eb71d2
KamilCzerniak <kamil.z.czerniak@gmail.com>
2018-10-22 10:12:43 +0200

    Completed

---
+++ 8-kyu/Convert a Boolean to a String/src/ConvertABooleanToAString.java
@@ -0,0 +1,10 @@
+public class ConvertABooleanToAString {
+
+    public static String convert(boolean b) {
+        return b ? "true" : "false";
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
