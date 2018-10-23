bde8055b177cbe3f857d058bd0046075fd58be32
KamilCzerniak <kamil.z.czerniak@gmail.com>
2018-10-23 21:18:08 +0200

    Completed

---
+++ 8-kyu/Welcome to the City/src/WelcomeToTheCity.java
@@ -0,0 +1,10 @@
+public class WelcomeToTheCity {
+
+    public String sayHello(String[] name, String city, String state) {
+        return String.format("Hello, %s! Welcome to %s, %s!", String.join(" ", name), city, state);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
