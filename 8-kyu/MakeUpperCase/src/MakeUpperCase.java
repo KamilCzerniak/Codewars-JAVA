ae5eda57ffc093db6900c5af4325796e156ed726
KamilCzerniak <kamil.z.czerniak@gmail.com>
2018-10-23 11:24:36 +0200

    Completed

---
+++ 8-kyu/MakeUpperCase/src/MakeUpperCase.java
@@ -0,0 +1,10 @@
+public class MakeUpperCase {
+
+    public static String MakeUpperCase(String str) {
+        return str.toUpperCase();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
