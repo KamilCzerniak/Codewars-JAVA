1b64ef3bde796612eb01043710e364e83e7572d0
KamilCzerniak <kamil.z.czerniak@gmail.com>
2018-10-23 14:27:30 +0200

    Completed

---
+++ 8-kyu/Basic variable assignment/src/BasicVariableAssignment.java
@@ -0,0 +1,10 @@
+public class BasicVariableAssignment {
+
+    public static String a = "code";
+    public static String b = "wa.rs";
+    public static String name = a + b;
+
+    public static void main(String[] args) {
+        System.out.println(name);
+    }
+}
