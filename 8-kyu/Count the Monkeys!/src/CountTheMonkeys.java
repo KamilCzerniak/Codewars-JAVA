4a1c303e3faa869cab84ab521111e166bc7ee385
KamilCzerniak <kamil.z.czerniak@gmail.com>
2018-10-23 14:18:23 +0200

    Completed

---
+++ 8-kyu/Count the Monkeys!/src/CountTheMonkeys.java
@@ -0,0 +1,16 @@
+public class CountTheMonkeys {
+
+    public static int[] monkeyCount(final int n) {
+
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = i + 1;
+        }
+        return a;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
