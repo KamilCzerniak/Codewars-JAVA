bd96679e0cb82d2f1d03b90c937dad1bc12db705
KamilCzerniak <kamil.z.czerniak@gmail.com>
2018-10-24 10:51:28 +0200

    Completed

---
+++ 8-kyu/Exclamation marks series #11 Replace all vowel to exclamation mark in the sentence/src/ExclamationMarksSeries11ReplaceAllVowelToExclamationMarkInTheSeentence.java
@@ -0,0 +1,10 @@
+public class ExclamationMarksSeries11ReplaceAllVowelToExclamationMarkInTheSeentence {
+
+    public static String replace(final String s) {
+        return s.replaceAll("[aeiouAEIOU]", "!");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
