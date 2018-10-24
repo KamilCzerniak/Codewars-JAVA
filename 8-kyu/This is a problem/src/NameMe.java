65278438ad9bb15f20c147a5810da6896718f71f
KamilCzerniak <kamil.z.czerniak@gmail.com>
2018-10-24 15:18:01 +0200

    Completed

---
+++ 8-kyu/This is a problem/src/NameMe.java
@@ -0,0 +1,24 @@
+public class NameMe {
+    public String firstName;
+    public String lastName;
+    public String fullName;
+
+    public NameMe(String first, String last) {
+        this.firstName = first;
+        this.lastName = last;
+        fullName = firstName + " " + lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+}
