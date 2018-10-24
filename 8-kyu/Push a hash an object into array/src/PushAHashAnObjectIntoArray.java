2936138c73c0071c4622be0726f31b38c33b8eda
KamilCzerniak <kamil.z.czerniak@gmail.com>
2018-10-24 12:01:13 +0200

    Completed

---
+++ 8-kyu/Push a hash an object into array/src/PushAHashAnObjectIntoArray.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PushAHashAnObjectIntoArray {
+    public static List push() {
+        List<String> items = new ArrayList<>();
+        items.add("an object");
+        return items;
+    }
+}
