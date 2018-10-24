a26ae4bcb35afc1d4ba2008bd1d4363726240d5c
KamilCzerniak <kamil.z.czerniak@gmail.com>
2018-10-24 12:13:44 +0200

    Completed

---
+++ 8-kyu/Training JS #7 if..else and ternary operator/src/TrainingJS7IfElseAndTernaryOperator.java
@@ -0,0 +1,10 @@
+public class TrainingJS7IfElseAndTernaryOperator {
+
+    public static int saleHotdogs(final int n) {
+        return n < 5 ? 100 * n : n < 10 ? 95 * n : 90 * n;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
