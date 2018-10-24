f757556a2e385f3eacbe0de15d804df93970c6f6
KamilCzerniak <kamil.z.czerniak@gmail.com>
2018-10-24 11:54:34 +0200

    Completed

---
+++ 8-kyu/Basic subclasses - Adam and Eve/src/God.java
@@ -0,0 +1,15 @@
+
+public class God {
+    public static Human[] create() {
+        return new Human[]{new Man(), new Woman()};
+    }
+}
+
+class Human {
+}
+
+class Man extends Human {
+}
+
+class Woman extends Human {
+}
