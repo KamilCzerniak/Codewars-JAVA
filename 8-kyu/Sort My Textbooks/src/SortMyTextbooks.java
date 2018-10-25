13cb93b471323c17b4c74499338a605d06bef072
KamilCzerniak <kamil.z.czerniak@gmail.com>
2018-10-25 17:13:24 +0200

    Completed

--- 8-kyu/Sort My Textbooks/src/SortMyTextbooks.java
+++ 8-kyu/Sort My Textbooks/src/SortMyTextbooks.java
@@ -1,9 +1,8 @@
-import java.util.Comparator;
 import java.util.List;
 
 public class SortMyTextbooks {
     public static List<String> sort(List<String> textbooks) {
-        textbooks.sort(Comparator.naturalOrder());
+        textbooks.sort(String.CASE_INSENSITIVE_ORDER);
         return textbooks;
     }
 }
