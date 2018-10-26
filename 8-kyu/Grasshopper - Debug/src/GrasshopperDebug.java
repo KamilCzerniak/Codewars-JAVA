aea9d7ec313dee6021426930b4ff5e384506fafa
KamilCzerniak <kamil.z.czerniak@gmail.com>
2018-10-27 01:16:51 +0200

    Completed

---
+++ 8-kyu/Grasshopper - Debug/src/GrasshopperDebug.java
@@ -0,0 +1,14 @@
+public class GrasshopperDebug {
+    public static String weatherInfo(int temp) {
+        double c = convertToCelsius(temp);
+        if (c > 0)
+            return (c + " is above freezing temperature");
+        else
+            return (c + " is freezing temperature");
+    }
+
+    public static double convertToCelsius(int temperature) {
+        return (temperature - 32) * (double) 5 / 9;
+    }
+}
+
