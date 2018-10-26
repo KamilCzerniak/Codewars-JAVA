2fa63266ff6702b05822a8143d32d561ff47effc
KamilCzerniak <kamil.z.czerniak@gmail.com>
2018-10-26 17:16:11 +0200

    Completed

---
+++ 8-kyu/A Strange Trip to the Market/src/AStrangeTripToTheMarket.java
@@ -0,0 +1,7 @@
+public class AStrangeTripToTheMarket {
+    public static boolean isLockNessMonster(String s) {
+        return s.contains("tree fiddy")
+                || s.contains("3.50")
+                || s.contains("three fifty");
+    }
+}
