2168fc47f372f95854cb881b56b09c6e9c539aa6
KamilCzerniak <kamil.z.czerniak@gmail.com>
2018-10-26 23:53:52 +0200

    Completed

---
+++ 8-kyu/Squash the bugs/src/SquashTheBugs.java
@@ -0,0 +1,12 @@
+public class SquashTheBugs {
+    public static int findLongest(final String str) {
+        String[] spl = str.split(" ");
+        int longest = 0;
+        for (int i = 0; i < spl.length; i++) {
+            if (spl[i].length() > longest) {
+                longest = spl[i].length();
+            }
+        }
+        return longest;
+    }
+}
