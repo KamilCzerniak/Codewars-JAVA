5784a0d627b99940f36496028f4184c79d8fda8d
Your NameKamilCzerniak <kamil.z.czerniak@gmail.com>
2018-10-29 13:46:45 +0100

    Removed file from repository

--- 8-kyu/Beginner - Reduce but Grow/src/BeginnerReduceButGrow.java
+++
@@ -1,9 +0,0 @@
-public class BeginnerReduceButGrow {
-    public static int grow(int[] x) {
-        int result = 1;
-        for (int d : x) {
-            result *= d;
-        }
-        return result;
-    }
-}
