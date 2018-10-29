b53101a9deee19b98bc70b6258576b2b03d339e5
Your NameKamilCzerniak <kamil.z.czerniak@gmail.com>
2018-10-29 12:35:37 +0100

    Completed

---
+++ 8-kyu/Beginner - Reduce but Grow/src/BeginnerReduceButGrow.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class BeginnerReduceButGrow {
+    public static int grow(int[] x) {
+        int result = 1;
+        for (int d : x) {
+            result *= d;
+        }
+        return result;
+    }
+}
