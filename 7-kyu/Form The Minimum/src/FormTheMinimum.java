6da55cea3017bc887e6cfbf96503092153106f86
KamilCzerniak <kamil.z.czerniak@gmail.com>
2018-10-31 02:11:41 +0100

    Completed

---
+++ 7-kyu/Form The Minimum/src/FormTheMinimum.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class FormTheMinimum {
+    public static int minValue(int[] values) {
+        return Integer.parseInt(Arrays.stream(values)
+                .distinct()
+                .sorted()
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining("")));
+    }
+}
