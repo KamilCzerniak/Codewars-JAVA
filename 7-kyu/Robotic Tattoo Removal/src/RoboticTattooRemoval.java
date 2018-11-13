20b3e79dddf1531858de0bc88d25bc72436c3fba
KamilCzerniak <kamil.z.czerniak@gmail.com>
2018-11-13 01:45:34 +0100

    Completed

---
+++ 7-kyu/Robotic Tattoo Removal/src/RoboticTattooRemoval.java
@@ -0,0 +1,12 @@
+public class RoboticTattooRemoval {
+    public static String[][] robot(String[][] skinScan) {
+        for (int i = 0; i < skinScan.length; i++) {
+            for (int j = 0; j < skinScan[i].length; j++) {
+                if (skinScan[i][j].equals("X")) {
+                    skinScan[i][j] = "*";
+                }
+            }
+        }
+        return skinScan;
+    }
+}
