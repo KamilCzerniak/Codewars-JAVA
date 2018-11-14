f45b3231d43a06b53c908267b58547bc6ba3e1fc
KamilCzerniak <kamil.z.czerniak@gmail.com>
2018-11-14 13:45:17 +0100

    Completed

---
+++ 7-kyu/Green Glass Door/src/GreenGlassDoor.java
@@ -0,0 +1,10 @@
+public class GreenGlassDoor {
+    public static boolean stepThroughWith(String s) {
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
