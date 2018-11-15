55ada042f37e7bc8970fa28a4e2f4d37bc3d3dbf
Kamil Czerniak <44313679+KamilCzerniak@users.noreply.github.com>
2018-11-15 04:50:59 +0100

    Update Solution.java

--- 7-kyu/Sum of differences between products and LCMs/src/Solution.java
+++ 7-kyu/Sum of differences between products and LCMs/src/Solution.java
@@ -6,10 +6,10 @@ public class Solution {
         return IntStream.range(0, pairs.length)
                 .reduce(0, (a, b) -> a + (pairs[b][0] * pairs[b][1] == 0 ? 0
                         : pairs[b][0] * pairs[b][1] - (pairs[b][0] * pairs[b][1])
-                        / gcb(pairs[b][0], pairs[b][1])));
+                        / gcd(pairs[b][0], pairs[b][1])));
     }
 
-    public static int gcb(int a, int b) {
-        return b == 0 ? a : gcb(b, a % b);
+    public static int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
     }
 }
