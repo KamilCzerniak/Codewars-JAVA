01347d897fc6eae07f3dad5911a49fbc5583276f
KamilCzerniak <kamil.z.czerniak@gmail.com>
2018-11-16 20:51:48 +0100

    Completed

---
+++ 7-kyu/Extra Perfect Numbers (Special Numbers Series #7)/src/Solution.java
@@ -0,0 +1,9 @@
+import java.util.stream.IntStream;
+
+public class Solution {
+    public static int[] extraPerfect(int number) {
+        return IntStream.rangeClosed(1, number)
+                .filter(i -> i % 2 != 0)
+                .toArray();
+    }
+}
