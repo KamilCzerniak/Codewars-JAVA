0f690651e2ac5613d7404d96a1d0c19d0597ee01
KamilCzerniak <kamil.z.czerniak@gmail.com>
2018-11-26 14:47:26 +0100

    Completed

---
+++ 7-kyu/Lombok Encapsulation/src/EncapsulationDemo.java
@@ -0,0 +1,14 @@
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class EncapsulationDemo {
+    private int number;
+    private String stringValue;
+    private Object anObject;
+}
