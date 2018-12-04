cb556ccda03359ca1982cb9edc48dc0b5a3e8b76
KamilCzerniak <kamil.z.czerniak@gmail.com>
2018-12-04 01:32:02 +0100

    Completed

---
+++ 6-kyu/Matrix Addition/src/Kata.java
@@ -0,0 +1,11 @@
+public class Kata {
+    public static int[][] matrixAddition(int[][] a, int[][] b) {
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                a[i][j] += b[i][j];
+            }
+        }
+        return a;
+    }
+}
