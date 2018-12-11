713d6e1bfe6ace8b0427dd0023824c33e843139c
KamilCzerniak <kamil.z.czerniak@gmail.com>
2018-12-11 02:02:42 +0100

    Completed

---
+++ 6-kyu/Ackermann Function/src/Solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public int Ackermann(int m, int n) {
+        if (m == 0) {
+            return n + 1;
+        } else if (m > 0 && n == 0) {
+            return Ackermann(m - 1, 1);
+        }
+        return Ackermann(m - 1, Ackermann(m, n - 1));
+    }
+
+}
