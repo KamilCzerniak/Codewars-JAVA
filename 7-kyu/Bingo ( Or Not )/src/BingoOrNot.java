f0444e83774f838c633744f192502647e4dcafee
KamilCzerniak <kamil.z.czerniak@gmail.com>
2019-01-07 00:47:54 +0100

    Completed

---
+++ 7-kyu/Bingo ( Or Not )/src/BingoOrNot.java
@@ -0,0 +1,13 @@
+
+package com.codewars.julesnuggy;
+import java.util.stream.IntStream;
+
+public class BingoOrNot {
+    public static String bingo(int[] numberArray) {
+        return IntStream.of(numberArray)
+                .distinct()
+                .filter(a -> String.valueOf((char) ('A' + a - 1))
+                        .matches("[BINGO]"))
+                .count() == 5 ? "WIN" : "LOSE";
+    }
+}
