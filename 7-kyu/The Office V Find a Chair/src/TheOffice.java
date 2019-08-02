2449dd75c03f28f8c3f7a641a3548650eef0004c
KamilCzerniak <kamil.z.czerniak@gmail.com>
2019-08-02 23:45:37 +0200

    Added theOffic class, first version

--- 7-kyu/The Office V Find a Chair/src/TheOffice.java
+++ 7-kyu/The Office V Find a Chair/src/TheOffice.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TheOffice {
+    public static Object meeting(Room[] x, int need) {
+
+        int chairs = 0;
+        int freeChairs;
+
+        List<Integer> list = new ArrayList<>();
+
+        if (need == 0) {
+            return "Game On";
+        }
+        for (Room room : x) {
+
+            int temp = room.chairs - room.occupants.length();
+
+            freeChairs = temp > 0 ? temp : 0;
+            chairs += freeChairs;
+
+            list.add(freeChairs);
+
+            if (chairs == need) {
+                return list.stream()
+                        .mapToInt(Integer::intValue)
+                        .toArray();
+            }
+        }
+        return "Not enough!";
+    }
 }
