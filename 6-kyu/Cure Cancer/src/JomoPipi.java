8a8347a25f8c63892b6231e3fbc42539216392db
KamilCzerniak <kamil.z.czerniak@gmail.com>
2019-08-06 19:36:58 +0200

    Completed

--- 6-kyu/Cure Cancer/src/JomoPipi.java
+++ 6-kyu/Cure Cancer/src/JomoPipi.java
@@ -2,23 +2,19 @@ import java.util.Arrays;
 
 class JomoPipi {
     public static int[] mutationLocation(char[][] body) {
-        int[] arr = Arrays.stream(body)
-                .mapToInt(c -> String.valueOf(c)
-                        .chars()
-                        .sum())
-                .toArray();
 
-        for (int i = 0; i < arr.length; i++) {
-            int row = arr[i];
-            if (Arrays.stream(arr).filter(n -> n == row).count() == 1) {
-                for (int j = 0; j < body[0].length; j++) {
-                    if (body[i][j] != body[i == body.length - 1 ? i - 1 : i + 1][j]) {
-                        return new int[]{i, j};
+
+        for (int j = 0; j < body.length; j += 1) {
+            if (!Arrays.equals(body[j], body[(j + 1) % body.length])
+                    && !Arrays.equals(body[j], body[(j + 2) % body.length])) {
+                for (int i = 0; i < body[j].length; i++) {
+                    if (body[j][i] != body[(j + 1) % body.length][i]) {
+                        return new int[]{j, i};
                     }
                 }
+
             }
         }
-
         return new int[]{};
     }
 }
